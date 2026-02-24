760cff5a783c0f6b4ecc1d2fea772bb79280f4ee
joparungaostudent-cell <joparungao.student@ua.edu.ph>
2026-02-24 13:43:37 +0800

    first commit

---
+++ cpeitel3prelim1/src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      <h1>Joshua Parungao</h1>
+      <div className="grid-container">
+        <div className="grid-item item1">1</div>
+        <div className="grid-item item2">2</div>
+        <div className="grid-item item3">3</div>
+        <div className="grid-item item4">4</div>
+        <div className="grid-item item5">5</div>
+        <div className="grid-item item6">6</div>
+        <div className="grid-item item7">7</div>
+        <div className="grid-item item8">8</div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
